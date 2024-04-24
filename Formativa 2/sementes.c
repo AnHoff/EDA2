43dbb79706e59af5c6d425162f7a42b16a22dfdf
Ana Hoffmann <ana.luiza.hoff@gmail.com>
2024-04-24 07:45:10 -0300

    Finaliza formativa 2

--- Formativa 2/sementes.c
+++ Formativa 2/sementes.c
@@ -1,122 +1,121 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct item_t {
-    long long unsigned identifier;
-    int quality_score;
-} item_t;
+#define ull unsigned long long
+#define less(a, b) (a.value == b.value ? a.key < b.key : a.value < b.value)
+#define less2(a, b) (a.key == b.key ? a.value < b.value : a.key < b.key)
+#define swap(a, b)  \
+    {               \
+        Item temp = a; \
+        a = b;      \
+        b = temp;      \
+    }
+#define cmpswap(a, b) \
+    if (less(b, a))   \
+    swap(a, b)
+#define cmpswap2(a, b) \
+    if (less2(b, a))   \
+    swap(a, b)
 
+typedef struct
+{
+    long long key;
+    int value;
+} Item;
 
-#define element_t item_t
-#define exchange(a, b) { element_t temp = a; a = b; b = temp; }
-
-
-typedef struct Priority_Queue {
-    int size;
-    int capacity;
-    element_t* array;
-    int (*compare)(element_t, element_t);
-} Priority_Queue;
-
-
-Priority_Queue* MinPriorityQueue_create(int max_N, int (*compare)(element_t, element_t)) {
-    Priority_Queue* pq = (Priority_Queue*)calloc(1, sizeof(Priority_Queue));
-    pq->array = (element_t*)calloc(max_N + 1, sizeof(element_t));
-    pq->size = 0;
-    pq->capacity = max_N;
-    pq->compare = compare;
-    return pq;
+int partition2(Item *arr, int left, int right)
+{
+    Item pivot = arr[right];
+    int i = left;
+    for (int j = left; j < right; j++)
+        if (less2(arr[j], pivot))
+        {
+            swap(arr[j], arr[i]);
+            i++;
+        }
+    swap(arr[i], arr[right]);
+    return i;
 }
 
-void MinPriorityQueue_destroy(Priority_Queue* pq) {
-    free(pq->array);
-    free(pq);
+void quicksortM3(Item *arr, int left, int right)
+{
+    if (right - left <= 32)
+        return;
+
+    swap(arr[(left + right) / 2], arr[right - 1]);
+    cmpswap2(arr[left], arr[right - 1]);
+    cmpswap2(arr[left], arr[right]);
+    cmpswap2(arr[right - 1], arr[right]);
+
+    int pivotIndex = partition2(arr, left, right);
+    quicksortM3(arr, left, pivotIndex - 1);
+    quicksortM3(arr, pivotIndex + 1, right);
 }
 
-void MinPriorityQueue_fix_up(Priority_Queue* pq, int k) {
-    element_t* v = pq->array;
-    while (k > 1 && (pq->compare)(v[k], v[k / 2])) {
-        exchange(v[k / 2], v[k]);
-        k = k / 2;
+void insertionsort(Item *arr, int left, int right)
+{
+    for (int i = left + 1, j; i <= right; i++)
+    {
+        Item key = arr[i];
+        for (j = i; j > 0 && less2(key, arr[j - 1]); j--)
+            arr[j] = arr[j - 1];
+        arr[j] = key;
     }
 }
 
-void MinPriorityQueue_fix_down(Priority_Queue* pq, int k) {
-    int j;
-    element_t* v = pq->array;
-    int size = pq->size;
-    while (k * 2 <= size) {
-        j = k * 2;
-        if (j < size && (pq->compare)(v[j + 1], v[j]))
-            j++;
-        if (!(pq->compare)(v[j], v[k]))
-            break;
-        exchange(v[k], v[j]);
-        k = j;
-    }
+void quicksort2(Item *arr, int left, int right)
+{
+    quicksortM3(arr, left, right);
+    insertionsort(arr, left, right);
 }
 
-void MinPriorityQueue_insert(Priority_Queue* pq, element_t element) {
-    if (pq->size + 1 >= pq->capacity) {
-        pq->capacity *= 2;
-        pq->array = realloc(pq->array, pq->capacity * sizeof(element_t));
-    }
-
-    pq->array[++pq->size] = element;
-    MinPriorityQueue_fix_up(pq, pq->size);
+int partition(Item *arr, int left, int right)
+{
+    Item pivot = arr[right];
+    int i = left;
+    for (int j = left; j < right; j++)
+        if (less(arr[j], pivot))
+        {
+            swap(arr[j], arr[i]);
+            i++;
+        }
+    swap(arr[i], arr[right]);
+    return i;
 }
 
-element_t MinPriorityQueue_delete_min(Priority_Queue* pq) {
-    element_t element = pq->array[1];
-    pq->array[1] = pq->array[pq->size--];
-    MinPriorityQueue_fix_down(pq, 1);
-    return element;
+void quickselect(Item *arr, int left, int right, int index)
+{
+    cmpswap(arr[(left + right) / 2], arr[right]);
+    cmpswap(arr[left], arr[(left + right) / 2]);
+    cmpswap(arr[right], arr[(left + right) / 2]);
+
+    int pivotIndex = partition(arr, left, right);
+    if (pivotIndex > index)
+        quickselect(arr, left, pivotIndex - 1, index);
+    else if (pivotIndex < index)
+        quickselect(arr, pivotIndex + 1, right, index);
 }
 
-int MinPriorityQueue_size(Priority_Queue* pq) {
-    return pq->size;
+Item array[10000001];
+
+void solve()
+{
+    int k;
+    scanf(" %d", &k);
+    int n;
+    for (n = 0; scanf(" %lld %d", &array[n].key, &array[n].value) == 2; n++);
+
+    quickselect(array, 0, n - 1, k - 1);
+    quicksort2(array, 0, k - 1);
+
+    for (int i = 0; i < k; i++)
+        printf("%lld %d\n", array[i].key, array[i].value);
 }
 
-
-int compare_by_score(element_t a, element_t b) {
-    if (a.quality_score != b.quality_score)
-        return a.quality_score < b.quality_score;
-    return a.identifier < b.identifier;
-}
-
-int compare_by_identifier(element_t a, element_t b) {
-    return a.identifier < b.identifier;
-}
-
-int main() {
-    int how_many_elements_to_print, i = 0, tmp;
-    int capacity = 1000;
-    item_t item;
-    Priority_Queue* pq_by_score = MinPriorityQueue_create(capacity, &compare_by_score);
-
-    scanf("%d\n", &how_many_elements_to_print);
-    Priority_Queue* pq_by_identifier = MinPriorityQueue_create(how_many_elements_to_print, &compare_by_identifier);
-
-    while (scanf("%lld %d\n", &item.identifier, &item.quality_score) != EOF) {
-        MinPriorityQueue_insert(pq_by_score, item);
-        ++i;
-    }
-
-    int items_length = i;
-    tmp = how_many_elements_to_print;
-    while (how_many_elements_to_print--) {
-        item = MinPriorityQueue_delete_min(pq_by_score);
-        MinPriorityQueue_insert(pq_by_identifier, item);
-    }
-
-    how_many_elements_to_print = tmp;
-    while (how_many_elements_to_print--) {
-        item = MinPriorityQueue_delete_min(pq_by_identifier);
-        printf("%lld %d\n", item.identifier, item.quality_score);
-    }
-
-    MinPriorityQueue_destroy(pq_by_score);
-    MinPriorityQueue_destroy(pq_by_identifier);
+int main()
+{
+    solve();
 
     return 0;
 }
